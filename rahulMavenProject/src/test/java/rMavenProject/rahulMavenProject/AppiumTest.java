480257686baf81364a44b3127dde80bbd4d64981
user <user@localhost>
2021-04-04 14:44:39 -0400

    maven project 1st commit

---
+++ rahulMavenProject/src/test/java/rMavenProject/rahulMavenProject/AppiumTest.java
@@ -0,0 +1,19 @@
+package rMavenProject.rahulMavenProject;
+
+import org.testng.annotations.Test;
+
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+
+	@Test
+	public void iOSaPP()
+	{
+		System.out.println("iOSaPP");
+	}
+}
---
+++ rahulMavenProject/src/test/java/rMavenProject/rahulMavenProject/RESTAPItest.java
@@ -0,0 +1,20 @@
+package rMavenProject.rahulMavenProject;
+
+import org.testng.annotations.Test;
+
+
+public class RESTAPItest {
+
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
